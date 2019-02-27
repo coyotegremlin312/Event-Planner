68daba1d46a63ba7aafd4024a38425b207200bcf
Ashley B <ashleybrown101789@gmail.com>
2019-02-27 15:43:34 -0800

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -133,35 +133,129 @@ class ComedyStore extends Component {
         <header className="EventHeader">
           Comedy Store Events
         </header>
-        <div className="The List">{this.state.comedianList}</div>
+        <div className="ComedyStoreList">{this.state.comedianList}</div>
       </div>
     );
   }
 }
 
 class IceHouse extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      comedianList: [],
+      comedians: '',
+      error: ''
+    };
+  }
+
+  componentDidMount = () => {
+    this.displayComedyBoard();
+  }
+
+  displayComedyBoard = (event) => {
+    const URL = "http://localhost:5000/events"
+    fetch(URL)
+      .then((res) => {
+        return res.json()
+      }).then((data) => {
+        console.log(data)
+        let comedians = [];
+        for(let i = 0; i < data.length; i++){
+          if(!data[i].date){
+            comedians.push(data[i])
+          }
+        }
+        this.setState({ comedians: comedians })
+      })
+      .then(() => {
+        const comedianList = this.state.comedians.map(this.createList)
+        this.setState({ comedianList, loading: false })
+      })
+  }
+
+  createList = (event, index) => {
+    return (
+      <div key={index} className="signature">
+        <h1 className="Performer">{event.performer}</h1>
+        <a href={event.link} className="Link">Event Info</a>
+      </div>
+    )
+  }
 
   render() {
+    if(this.state.loading === true){
+      return "Loading"
+    }
     return (
       <div className="EventList">
         <header className="EventHeader">
         Ice House Events
         </header>
-        <div className="The List"></div>
+        <div className="IceHouseList">{this.state.comedianList}</div>
       </div>
     );
   }
 }
 
 class Ticketmaster extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      performerList: [],
+      performers: '',
+      error: ''
+    };
+  }
+
+  componentDidMount = () => {
+    this.displayEventsBoard();
+  }
+
+  displayEventsBoard = (event) => {
+    const URL = "http://localhost:5000/ticketmasterevents"
+    fetch(URL)
+      .then((res) => {
+        return res.json()
+      }).then((data) => {
+        console.log(data)
+        let performers = [];
+        for(let i = 0; i < data.length; i++){
+            performers.push(data[i])
+        }
+        this.setState({ performers: performers })
+      })
+      .then(() => {
+        const performerList = this.state.performers.map(this.createList)
+        this.setState({ performerList, loading: false })
+      })
+  }
+
+  createList = (event, index) => {
+    return (
+      <div key={index} className="signature">
+        <h1 className="Performer">{event.name}</h1>
+        <h2 className="Location">{event.location}</h2>
+        <h2 className="Date">{event.date}</h2>
+        <a href={event.link} className="Link">Event Info</a>
+      </div>
+    )
+  }
 
   render() {
+    if(this.state.loading === true){
+      return "Loading"
+    }
     return (
       <div className="EventList">
         <header className="EventHeader">
         TicketMaster Events
         </header>
-        <div className="The List"></div>
+        <div className="TicketMasterList">{this.state.performerList}</div>
       </div>
     );
   }
