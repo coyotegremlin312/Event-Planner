361be690fc67424096aa89c3f87dd410acf9412d
Ashley B <ashleybrown101789@gmail.com>
2019-02-28 17:11:36 -0800

    somedy

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ const scrapeController = require('./scraper.js');
 const search = require('./ticketmaster.js');
 const getNames = require("./firebaseServer.js");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', (req, res) => {
-    res.setHeader("Content-Type", "application/json");
     res.send('It is on like Donkey Kong');
-    res.end();
 })
 
 app.get('/events', 
